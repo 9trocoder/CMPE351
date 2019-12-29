73ebec34d3b4075afa5dc15ec4f850349a5bd285
yemicodes <yemicodes@gmail.com>
2019-12-29 09:08:16 +0300

    editing scheduling.cpp adding struct to it

--- scheduling.cpp
+++ scheduling.cpp
@@ -3,12 +3,26 @@
 #include<cstdlib>
 using namespace std;
 
-struct processingdata{
+struct Processing_data{
     int burst_time, arrival_time, priority;
 };
 
 int main (){
-    cout<<"Hello world";
+    Processing_data processing_datas[100];
+    ifstream fin("input.txt");
+    if(!fin) exit(1);
+
+    Processing_data pd;
+    int number_of_pd = 0;
+    fin >> pd.burst_time;
+    while(!(pd.burst_time == -1)){
+        fin >> pd.arrival_time >> pd.priority;
+        processing_datas[number_of_pd] = pd;
+        number_of_pd++;
+    }
+    fin.close();
+
+
 
     return 0;
 }
