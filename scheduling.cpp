fa1b32ecd866c16de2540465dd006d2c8957572f
yemicodes <yemicodes@gmail.com>
2019-12-29 08:54:44 +0300

    added new codes

--- scheduling.cpp
+++ scheduling.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
+#include<fstream>
+#include<cstdlib>
 using namespace std;
+
+struct processingdata{
+    int burst_time, arrival_time, priority;
+};
+
 int main (){
     cout<<"Hello world";
 
