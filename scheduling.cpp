f1b2b2e9c5a94a4966d86a0486f20fc68afb078d
yemicodes <yemicodes@gmail.com>
2019-12-29 20:47:43 +0300

    project

--- scheduling.cpp
+++ scheduling.cpp
@@ -18,6 +18,21 @@ void printmessage() {
 }
 
 int main (){
+    int process_number;
+    int burst_time;
+    int arrival_time;
+    int priority;
+    struct node* start = NULL;
+
+    ifstream fin("input.txt");
+    if(!fin) exit(1);
+
+    while(fin >> process_number >> arrival_time>> burst_time >> priority)
+    {
+        start = take_back(start, process_number, arrival_time, burst_time, priority);
+    }
+    fin.close();
+
     printmessage();
 
     return 0;
