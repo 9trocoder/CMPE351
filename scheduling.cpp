28397846a8d228f25eda1e7b943120a75a44ff04
yemicodes <yemicodes@gmail.com>
2019-12-29 16:17:19 +0300

    writing the option message

--- scheduling.cpp
+++ scheduling.cpp
@@ -3,26 +3,24 @@
 #include<cstdlib>
 using namespace std;
 
-struct Processing_data{
-    int burst_time, arrival_time, priority;
-};
+void printmessage() {
+
+    int option;
+    cout<<"\t\t\t"<<"CPU Scheduler Similator"<<endl;
+    cout<<"1) Scheduling Method"<<endl;
+    cout<<"2) Preemptive Mode"<<endl;
+    cout<<"3) Non-preemptive Mode"<<endl;
+    cout<<"4) Show Result"<<endl;
+    cout<<"5) End Program"<<endl;
+    cout<<"Option"<<endl;
+    cin>>option;
+
+}
 
 int main (){
-    Processing_data processing_datas[100];
-    ifstream fin("input.txt");
-    if(!fin) exit(1);
-
-    Processing_data pd;
-    int number_of_pd = 0;
-    fin >> pd.burst_time;
-    while(!(pd.burst_time == -1)){
-        fin >> pd.arrival_time >> pd.priority;
-        processing_datas[number_of_pd] = pd;
-        number_of_pd++;
-    }
-    fin.close();
-
-
+    printmessage();
 
     return 0;
 }
+
+
